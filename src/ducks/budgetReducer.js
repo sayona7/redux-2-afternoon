c9cfbd7601788d8e660b694780a20c0120f5816f
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-18 22:40:25 -0500

    solved first 2 steps

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+        purchases: [],
+        budgetLimit: null,
+        loading: false
+}
+
+
+export default function reducer (state = initialState, action) {
+    return state;
+}
