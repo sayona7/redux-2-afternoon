4e9ad0a1c324756082690251ceec934ae84b91c9
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-19 08:39:16 -0500

    solved steps 5 & 6

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -8,25 +8,32 @@ import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
 import { connect } from 'react-redux';
-
+import {requestUserData} from "./../../ducks/userReducer";
+import {requestBudgetData} from "./../../ducks/budgetReducer";
 
 class Budget extends Component {
 
+  componentDidMount() {
+    this.props.requestUserData();
+    this.props.requestBudgetData();
+  }
+
   render() {
-    const {loading} = this.props.budget;
+    const {loading, purchases, budgetLimit} = this.props.budget;
+    const {firstName, lastName} = this.props.user;
     return (
       <Background>
         {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
               <AddPurchase />
-              <DisplayPurchases />
+              <DisplayPurchases purchases={purchases} />
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit} />
+              <Chart2 purchases={purchases} />
             </div>
           </div>
         </div>
@@ -37,8 +44,9 @@ class Budget extends Component {
 
 function mapStateToProps(state) {
   return {
-    budget: state.budget
+    budget: state.budget,
+    user: state.user
   }
 }
 
-export default connect(mapStateToProps)(Budget);
+export default connect(mapStateToProps, {requestUserData, requestBudgetData})(Budget);
--- src/ducks/budgetReducer.js
+++ src/ducks/budgetReducer.js
@@ -1,10 +1,29 @@
+import axios from "axios";
+
+const REQUEST_BUDGET_DATA = "REQUEST_BUDGET_DATA"
+
 const initialState = {
         purchases: [],
         budgetLimit: null,
         loading: false
 }
 
+export const requestBudgetData = () => {
+    let data = axios.get("/api/budget-data").then(res => res.data)
+    return {
+        type: REQUEST_BUDGET_DATA,
+        payload: data
+    }
+}
+
 
 export default function reducer (state = initialState, action) {
-    return state;
+    switch(action.type) {
+        case REQUEST_BUDGET_DATA + "_PENDING":
+            return {...state, loading: true}
+        case REQUEST_BUDGET_DATA + "_FULFILLED":
+            return {...state, ...action.payload, loading: false}
+        default:
+            return state;
+    }
 }
