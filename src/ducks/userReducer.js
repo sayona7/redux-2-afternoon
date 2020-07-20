33adb87d7aa38c690e0d3cdd2fe3f96c46e319bd
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-07-19 19:52:40 -0500

    added some corrections

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -6,10 +6,10 @@ const initialState = {
     lastName: null
 };
 
-const REQUEST_USER_DATA = "REQUEST_USER_DATA"
+const REQUEST_USER_DATA = "REQUEST_USER_DATA";
 
 export const requestUserData = () => {
-    let data = axios.get("/auth/user-data").then(res => res.data)
+    let data = axios.get("/auth/user-data").then(res => res.data);
     return {
         type: REQUEST_USER_DATA,
         payload: data
